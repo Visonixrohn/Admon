3230d236ac2c2196961a48b4ff22eafb473b9b42
Visonixrohn <visonixro@gmail.com>
2025-12-05 20:33:21 +0000

    chore(pwa): set manifest start_url to / and register sw with root scope

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,13 +9,11 @@ if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
 		const swUrl = '/sw.js';
 		navigator.serviceWorker
-			.register(swUrl)
+			.register(swUrl, { scope: '/' })
 			.then((reg) => {
-				// eslint-disable-next-line no-console
 				console.log('Service worker registered:', reg.scope);
 			})
 			.catch((err) => {
-				// eslint-disable-next-line no-console
 				console.warn('Service worker registration failed:', err);
 			});
 	});
